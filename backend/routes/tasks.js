707046e205ff6e0d2906ad3fbd1aed6d090f226f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-08-23 22:43:28 +0500

    task manager api

---
+++ backend/routes/tasks.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  getTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
+const router = express.Router();
+
+router.route("/").get(getTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+
+// const notFound = require("./middleware/not-found");
+// const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/tasks", require("./routes/tasks"));
+// app.use(notFound);
+// app.use(errorHandlerMiddleware);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
